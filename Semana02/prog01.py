3fd4e0f1b22668b7f7d0df81f4da559c6a39a759
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-17 16:18:39 -0300

    Uso do len

--- Semana02/prog01.py
+++ Semana02/prog01.py
@@ -1,11 +1,2 @@
-message = 'Bobby\'s World' #Usando o \ para pular a aspas simples
-#message = "Bobby's World" #Usando "" para que a string contiue completa
-#Alternativamente se houver uma citação, que contém aspas duplas,
-# o uso de aspas simples para designar a string é muito útil
-#Ou ainda posso usar:
-# message = """Bobby's World was a good 
-#cartoon in the 1990s"""
-#para criar uma string multi linha
-print(message)
-
-
+message = 'Hello World'
+print(len (message)) #para descobrir quantos caracteres a string possui
