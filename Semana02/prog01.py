66ff587e26c188b0ce3503359d917c6dbae9711a
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-17 16:08:46 -0300

    Gerenciamento das aspas

--- Semana02/prog01.py
+++ Semana02/prog01.py
@@ -1,3 +1,6 @@
-
-message  = 'Hello World'
+message = 'Bobby\'s World' #Usando o \ para pular a aspas simples
+#message = "Bobby's World" #Usando "" para que a string contiue completa
+#Alternativamente se houver uma citação, que contém aspas duplas, o uso de aspas simples para designar a string é muito útil
 print(message)
+
+
