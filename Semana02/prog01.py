a488069f1d15a3ef34382617017f778c53c7f48f
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-17 19:25:24 -0300

    Strings Formatadas

--- Semana02/prog01.py
+++ Semana02/prog01.py
@@ -1,7 +1,7 @@
 #Métodos são funcões que pertencem a um objeto
 #Concatenação
+#Uma outra forma, para strings longas é melhor usar uma string formatada
 greeting = 'Hello'
 name = 'Ana Júlia'
-#message = greeting + name ##esta forma apenas junta as strings uma na frente a outra
-message = greeting + ', ' + name #é possível resolver adicionando um string de espaço ''
+message = '{}, {}. Welcome!'.format(greeting,name) #Os {} são os placeholders, onde as variáveis serão distribuidas
 print(message)
