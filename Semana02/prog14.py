a46a9527bef17ea2c2c48566fb1d221383d67057
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 14:50:36 -0300

    Adicionar fonte de referência dos atalhos

--- Semana02/prog14.py
+++ Semana02/prog14.py
@@ -11,6 +11,7 @@ for f in os.listdir():
     #Pesquisar o atalho mostrado no vídeo
     #Alt + botaõ esquerdom mouse para selecionar
     #ctrl + shift+ alt +setas
+    #fonte : https://pt.stackoverflow.com/questions/233417/sele%C3%A7%C3%A3o-multi-linha-no-vs-code#:~:text=Isso%20pode%20ser%20feito%20usando,%E2%86%92%20ou%20%E2%86%93%20ou%20%E2%86%90%20).
     new_name = '{}-{}{}'.format(f_num,f_title,f_ext)
 
     #os.rename(f, new_name) #deixei comentada para não alterar o arquivo novemente
