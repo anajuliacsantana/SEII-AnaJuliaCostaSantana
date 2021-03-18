9cf79b1e475d14ed1b32f2bc156f14497d46da1d
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 01:54:20 -0300

    Invertendo a lista-Reverse

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -1,7 +1,6 @@
 courses = ['History','Math','Physics','CompSci']
 
-popped = courses.pop()
-#Remove o último valor da lista, e retorna o valor removido
-print(popped)
+courses.reverse()
+
 print(courses)
 
