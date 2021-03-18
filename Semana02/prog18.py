a48359a50b6f9e586d4677f537a714184d6fea3a
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 20:09:29 -0300

    LEGB

--- Semana02/prog18.py
+++ Semana02/prog18.py
@@ -7,9 +7,10 @@ Local, Enclosing, Global, Built-in
 # x = 'global x'
 
 def outer():
-    # x = 'outer x'
+    x = 'outer x'
     def inner():
-        x = 'inner x'
+        nonlocal x
+        # x = 'inner x'
         print(x)
     inner()
     print(x)
