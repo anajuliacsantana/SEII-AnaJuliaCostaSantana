8b0370cc367d59fae81893c903af8dc9e7d44125
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 03:15:42 -0300

    Dicionários-Deletar informação

--- Semana02/prog05.py
+++ Semana02/prog05.py
@@ -1,7 +1,5 @@
 
 student = {'name': 'John','age': 25,'courses':['Math', 'CompSci']}
 
-student.update({'name': 'Jane','age':26,'phone':'555-5555'})
-
-#print(student.get('phone','Not Found'))
+del student['age']
 print(student)
