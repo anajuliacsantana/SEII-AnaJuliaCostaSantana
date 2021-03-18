d8d3fe7e4447d9ee319f73e6e56afb3d1e5d1467
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 09:55:41 -0300

    Funções-Mantém o programa DRY

--- Semana02/prog08.py
+++ Semana02/prog08.py
@@ -1,10 +1,9 @@
 
 def hello_func():
-    print('Hello Funcion.') #A alteração pode ser feita apenas na função
+   return 'Hello Funcion.' #A alteração pode ser feita apenas na função
 
 hello_func()
 hello_func()
 hello_func()
 hello_func()
-
 
