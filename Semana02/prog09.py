8da713c1aad1c37c62f4872ee3f7afe43053f509
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 10:34:39 -0300

    Importando variavél da função

--- Semana02/prog09.py
+++ Semana02/prog09.py
@@ -1,7 +1,8 @@
 
-from my_module import find_index
+from my_module import find_index,test
 
 courses = ['History','Math','Physics','CompSci']
 
 index = find_index(courses,'Math')
 print(index)
+print(test)
