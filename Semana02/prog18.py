1c26dcbb7bfd4f59d09eb790358002aed47dd0d9
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 20:06:33 -0300

    LEGB

--- Semana02/prog18.py
+++ Semana02/prog18.py
@@ -4,23 +4,16 @@ LEGB
 Local, Enclosing, Global, Built-in
 """
 
-import builtins
-
-
-# print(dir(builtins))
-
-def my_min():
-    pass
-m = min([5,1,4,2,3])
-print(m)
-
 # x = 'global x'
 
-def test(z):
-    global x
-    x = 'local x'
-    #print(y)
-    print(z)
-# test('local z')
+def outer():
+    x = 'outer x'
+    def inner():
+        x = 'inner x'
+        print(x)
+    inner()
+    print(x)
+outer()
 
-# print(z)
+
+# print(x)
