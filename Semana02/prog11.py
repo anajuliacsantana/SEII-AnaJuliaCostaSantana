ef35a7c25649da290a8fe4a36495fdcf0677762a
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 12:47:19 -0300

    Escrevendo arquivos

--- Semana02/prog11.py
+++ Semana02/prog11.py
@@ -1,16 +1,7 @@
 #File Objects
 
 with open('test.txt','r') as f:
-    
-    size_to_read = 10
-
-    f_contents = f.read(size_to_read)
-    print(f_contents, end ='')
-
-    f.seek(0)
-
-    f_contents = f.read(size_to_read)
-    print(f_contents)
+    f.write('Test')
 
     # while len(f_contents)>0:
     #         print(f_contents, end ='')
