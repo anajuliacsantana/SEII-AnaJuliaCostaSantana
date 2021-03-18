62afb809e38b3abfc4272c90993a5640726b0ba0
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 19:07:19 -0300

    Trabalhando com Datas

--- Semana02/prog17.py
+++ Semana02/prog17.py
@@ -1,8 +1,6 @@
 import datetime
 
 tday = datetime.date.today()
-print(tday.month)
-#no código acima posso selecionar
-#ano : .year
-#dia : .day
-#mês :.month
+print(tday.weekday()) #Monday 0 Sunday 6
+print(tday.isoweekday()) #Monday 1 Sunday 7
+
