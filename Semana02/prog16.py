530c5beef330570f38edec976a97feab88bd8521
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 16:38:33 -0300

    Enumerando para o htm

--- Semana02/prog16.py
+++ Semana02/prog16.py
@@ -6,15 +6,18 @@ names = []
 with open('patrons.csv','r') as data_file:
     csv_data = csv.DictReader(data_file)
 
-    #We dont' want headers or first line of band data
-    next(csv_data)
-    next(csv_data)
+    for item in csv_data:
+        print(item)
+    
+    # #We dont' want headers or first line of band data
+    # next(csv_data)
+    # next(csv_data)
 
    
-    for line in csv_data:
-        if line[0] == 'No Reward':
-            break
-        names.append(f"{line[0]} {line[1]}" ) 
+    # for line in csv_data:
+    #     if line[0] == 'No Reward':
+    #         break
+    #     names.append(f"{line[0]} {line[1]}" ) 
 
 
 html_output +=f'<p>There are currently {len(names)} public contributors. Thank You!</p>'
@@ -24,7 +27,7 @@ html_output += '\n<ul>'
 for name in names:
     html_output += f'\n\t<li>{name}</li>'
 
-htmç_output += '\n</ul>'
+html_output += '\n</ul>'
 
-print(html_output)
+# print(html_output)
 
