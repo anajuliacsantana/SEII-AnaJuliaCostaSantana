e7341bf66b0fd050b9dd64538b6ed097cb3f2ef2
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-17 21:42:41 -0300

    Números como strings

--- Semana02/prog03.py
+++ Semana02/prog03.py
@@ -1,18 +1,7 @@
-# Arithmetic Operators:
-# Addition:       3 + 2
-# Subtraction:    3 - 2
-# Multiplication: 3 * 2
-# Division:       3 / 2
-# Floor Division: 3 // 2
-# Exponent:       3 ** 2
-# Modulus:        3 % 2
+#Números em Strings
 
+num_1 = '100'
+num_2 = '200'
 
-# Comparisons:
-# Equal:            3 == 2
-# Not Equal:        3 != 2
-# Greater Than:     3 > 2
-# Less Than:        3 < 2
-# Greater or Equal: 3 >= 2
-# Less or Equal:    3 <= 2
+print(num_1+num_2)
 
