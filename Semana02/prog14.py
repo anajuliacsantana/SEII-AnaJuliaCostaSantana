98708f24c7cb5a56a383e14ee5bf55d6c9767ed8
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 14:36:33 -0300

    Trocando a ordem do nome

--- Semana02/prog14.py
+++ Semana02/prog14.py
@@ -3,5 +3,14 @@ import os
 os.chdir('/Users/anaju/Documents/Playlist')
 
 for f in os.listdir():
-    print(os.path.splitext(f))
+    f_name, f_ext = os.path.splitext(f)
+    f_title,f_course, f_num = f_name.split('-')
+    f_title = f_title.strip()
+    f_course = f_course.strip()
+    f_num = f_num.strip().zfill(2)
+    #Pesquisar o atalho mostrado no vídeo
+    #Alt + botaõ esquerdom mouse para selecionar
+    #ctrl + shift+ alt - setas
+    print('{}-{}-{}'.format(f_num,f_course,f_title,f_ext))
+
 
