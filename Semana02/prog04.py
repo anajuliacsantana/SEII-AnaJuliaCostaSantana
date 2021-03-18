7ac9346ffb4164ade423b213c286874fb8526ed1
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 01:38:07 -0300

    Usos do Index

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -1,9 +1,8 @@
 courses = ['History','Math','Physics','CompSci']
 
-print(courses[0:2])
-#Printa todos os valores de 0 e para antes do segundo index
-#por isso o intervalo apresenta 2 itens
-#pode ser iniciada como :2
+print(courses[2:])
+#Printa todos os valores a partir do primeiro
+#index até o final da lista
 
 
 
