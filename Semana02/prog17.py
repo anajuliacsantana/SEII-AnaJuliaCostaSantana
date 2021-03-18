a9b4fd9f87215c16097d300e234c236c49f5500d
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 19:43:38 -0300

    Módulo datetime e pytz

--- Semana02/prog17.py
+++ Semana02/prog17.py
@@ -3,14 +3,5 @@ import pytz
 
 
 
-dt_utcnow = datetime.datetime.now(tz=pytz.UTC)
-# print(dt_utcnow)
-
-dt_mtn = datetime.datetime.now()
-mtn_tz = pytz.timezone('US/Mountain')
-
-dt_mtn =mtn_tz.localize(dt_mtn)
-# dt_east = dt_mtn.astimezone(pytz.timezone('US/Eastern'))
-
-print(dt_mtn)
-
+dt_mtn = datetime.datetime.now(tz=pytz.timezone('US/Mountain'))
+print(dt_mtn.isoformat())
