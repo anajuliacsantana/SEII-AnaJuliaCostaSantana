b75c1ab0cce9513a0ebaf825f6c2ba40ec6512a3
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 10:36:39 -0300

    Importando tudo

--- Semana02/prog09.py
+++ Semana02/prog09.py
@@ -1,8 +1,9 @@
 
-from my_module import find_index as fi,test
+from my_module import *
 
 courses = ['History','Math','Physics','CompSci']
 
-index = fi(courses,'Math')
+index = find_index(courses,'Math')
 print(index)
 print(test)
+#Não há como saber o que veio da função importada
