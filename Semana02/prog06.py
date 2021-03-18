5dca35dfa36d5272d8ae1170485552a244d88990
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 09:05:11 -0300

    Condicionais na função do switch case

--- Semana02/prog06.py
+++ Semana02/prog06.py
@@ -7,12 +7,14 @@
 # Greater or Equal: >=
 # Less or Equal:    <=
 # Object Identity:  is #checa se os objetos tem o mesmo id
-
+#Java não tem um switch case
+#Posso usar o if else e elif no lugar
 language = 'Java'
 if  language == 'Python':
     print('Language is Python')
 elif language == 'Java':
     print('Language is Java')
-
+elif language == 'JavaScript':
+    print('Language is JavaScript')
 else:
     print('No match')
