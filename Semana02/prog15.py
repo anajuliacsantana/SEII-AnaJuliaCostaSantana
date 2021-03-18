99ec3dce1de2fca3300849e74e4ca76412e2c17e
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 15:26:59 -0300

    Informando o delimitador

--- Semana02/prog15.py
+++ Semana02/prog15.py
@@ -1,11 +1,14 @@
 import csv
 
-with open('names.csv','r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+with open('new_names.csv','r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter= '\t')
 
-    with open('new_names.csv','w') as new_file:
-        csv_writer = csv.writer(new_file, delimiter = '\t')
+    for line in csv_reader:
+        print(line)
+
+    # with open('new_names.csv','w') as new_file:
+    #     csv_writer = csv.writer(new_file, delimiter = '\t')
         
-        for line in csv_reader:
-            csv_writer.writerow(line)
+    #     for line in csv_reader:
+    #         csv_writer.writerow(line)
 
