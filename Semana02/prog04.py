a0be2d87b0a6a2f0b99aa4bc4a727a55708ee47d
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 02:57:48 -0300

    Set5-Combinação geral, union

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -1,8 +1,7 @@
 
-
 # Sets
 cs_courses = {'History', 'Math', 'Physics', 'CompSci', 'Math'}
 art_courses = {'History', 'Math', 'Art', 'Design'}
 
 
-print(cs_courses.difference(art_courses))
+print(cs_courses.union(art_courses))
