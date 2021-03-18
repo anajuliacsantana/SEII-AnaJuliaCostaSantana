4574f4d1b56e5d83c5d5bf40543e7d8528504095
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 02:56:24 -0300

    Set4-Palavras  apenas  um dos sets,debate saudável

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -1,6 +1,8 @@
+
+
 # Sets
 cs_courses = {'History', 'Math', 'Physics', 'CompSci', 'Math'}
 art_courses = {'History', 'Math', 'Art', 'Design'}
 
-print(cs_courses.intersection(art_courses))
 
+print(cs_courses.difference(art_courses))
