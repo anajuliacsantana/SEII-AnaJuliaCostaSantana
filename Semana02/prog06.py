07da345cdfff1009c78b9f4978348d7ba430f226
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 09:01:16 -0300

    Condicionais-Fale:Exemplo

--- Semana02/prog06.py
+++ Semana02/prog06.py
@@ -1,4 +1,15 @@
 
-language = 'Python'
+# Comparisons:
+# Equal:            ==
+# Not Equal:        !=
+# Greater Than:     >
+# Less Than:        <
+# Greater or Equal: >=
+# Less or Equal:    <=
+# Object Identity:  is #checa se os objetos tem o mesmo id
+
+language = 'Java'
 if  language == 'Python':
-    print('Conditional was True')
+    print('Language is Python')
+else:
+    print('No match')
