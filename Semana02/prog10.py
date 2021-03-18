9494240b1849896682c4937efc5fc8319ed7f3ea
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 12:14:52 -0300

    Módulo os-path basename

--- Semana02/prog10.py
+++ Semana02/prog10.py
@@ -1,12 +1,9 @@
 import os
 
 from datetime import datetime
+
 os.chdir('/Users/anaju/Desktop')
 
-print(os.environ.get('HOMEPATH'))
+# print(os.environ.get('HOMEPATH'))
 
-'test.txt'
-
-file_path = os.path.join(os.environ.get('HOMEPATH'),'test.txt')
-print(file_path)
-
+print(os.path.basename('/tmp/test.txt'))
