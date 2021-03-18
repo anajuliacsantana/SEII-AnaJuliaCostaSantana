a3a371f9f024acb163b1847af845eff1c46a59fe
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 01:45:13 -0300

    Adicionando muitos valores-Dificuldade?

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -1,6 +1,7 @@
 courses = ['History','Math','Physics','CompSci']
 
-courses.insert(0,'Art')#argumentos, a posição e o que será acrescentado
-
+courses_2 = ['Art','Education']
+courses.insert(0,courses_2)
+#Adiciona toda a ista e não os valores individuais
 print(courses)
 
