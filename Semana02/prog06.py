5e2720e3e9a908f08cc332975c5bd3f8487b2681
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 09:13:49 -0300

    Diferença is e ==, uso do ==

--- Semana02/prog06.py
+++ Semana02/prog06.py
@@ -1,18 +1,5 @@
 
+a = [1,2,3]
+b = [1,2,3]
 
-
-#and
-#Ambas as condições devem ser verdadeiras
-#or
-#Uma ou outra informação pode ser Verdadeira
-#not
-#Operação booleana, para trocar Veradeiro para Falo e Falso para Verdadeiro
-
-user = 'Admin'
-logged_in = False
-
-if not logged_in:
-    print('Please Log In')
-else:
-    print('Welcome')
-
+print(a ==b)
