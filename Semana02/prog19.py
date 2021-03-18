33cb61d1db63e855d2b633b0d502a1b240906c19
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 20:26:14 -0300

    Organizando-Listas

--- Semana02/prog19.py
+++ Semana02/prog19.py
@@ -1,10 +1,12 @@
 
-#Sort metod works only in lists
-tup = (9, 1, 8, 2, 7, 3, 6, 4, 5)
-s_tup = sorted(tup)
-print('Tuple\t',s_tup)
+#Sort metod works only in lists, logo converte os outros dados
+# tup = (9, 1, 8, 2, 7, 3, 6, 4, 5)
+# s_tup = sorted(tup)
+# print('Tuple\t',s_tup)
 
-
+di = {'name':'Corey','job':'programming','age': None, 'os':'Mac'}
+s_di = sorted(di)
+print('Dict\t',s_di)
 
 
 
