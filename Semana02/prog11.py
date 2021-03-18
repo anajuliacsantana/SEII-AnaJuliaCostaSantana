6832c5f0cdee551e8874715bd3906b80cd86eac5
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 12:53:14 -0300

    Copiando arquivos

--- Semana02/prog11.py
+++ Semana02/prog11.py
@@ -1,6 +1,6 @@
 #File Objects
 
-with open('test2.txt','w') as f:
-    f.write('Test')
-    f.seek(0)
-    f.write('R')
+with open('test.txt','r') as rf:
+   with open('test_copy.txt','w') as wf:
+       for line in rf:
+           wf.write(line)
