a7ada52c0b535e32edaa496eee0a6e95ab727456
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 12:58:43 -0300

    Copiando imagens

--- Semana02/prog11.py
+++ Semana02/prog11.py
@@ -1,6 +1,6 @@
 #File Objects
 
-with open('panda-deitado.jpg','r') as rf:
-   with open('panda-deitado-copy.jpg','w') as wf:
+with open('panda-deitado.jpg','rb') as rf:
+   with open('panda-deitado-copy.jpg','wb') as wf:
        for line in rf:
            wf.write(line)
