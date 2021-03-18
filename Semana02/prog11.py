0f2dd5fe9c0e6a06832d5b4ecf3c2e2286534d32
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 12:37:16 -0300

    Abrindo arquivos

--- Semana02/prog11.py
+++ Semana02/prog11.py
@@ -2,7 +2,7 @@
 
 with open('test.txt','r') as f:
     f_contents = f.readline()
-    print(f_contents)
+    print(f_contents, end ='')
     f_contents = f.readline()
-    print(f_contents)
+    print(f_contents, end ='')
 
