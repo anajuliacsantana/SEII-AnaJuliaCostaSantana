1da5426f0302b3c3dac60b529a06829d417612cb
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 09:08:25 -0300

    Condicionais com and

--- Semana02/prog06.py
+++ Semana02/prog06.py
@@ -1,20 +1,15 @@
 
-# Comparisons:
-# Equal:            ==
-# Not Equal:        !=
-# Greater Than:     >
-# Less Than:        <
-# Greater or Equal: >=
-# Less or Equal:    <=
-# Object Identity:  is #checa se os objetos tem o mesmo id
-#Java não tem um switch case
-#Posso usar o if else e elif no lugar
-language = 'Java'
-if  language == 'Python':
-    print('Language is Python')
-elif language == 'Java':
-    print('Language is Java')
-elif language == 'JavaScript':
-    print('Language is JavaScript')
+
+
+#and
+#or
+#not
+
+user = 'Admin'
+logged_in = False
+
+if user == 'Admin' and logged_in:
+    print('Admin Page')
 else:
-    print('No match')
+    print('Bad Creds')
+
