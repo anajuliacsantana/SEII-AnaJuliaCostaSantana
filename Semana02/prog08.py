cf9a69ce59bb5d6059fdd24bcce6c3bf2a03a6c4
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 10:08:56 -0300

    Funções-Exemplo args e kwargs

--- Semana02/prog08.py
+++ Semana02/prog08.py
@@ -2,4 +2,10 @@
 def hello_func(greeting,name = 'You'):
    return '{}, {}' .format(greeting,name)
 
-print(hello_func('Hi',name = 'Corey'))
+# print(hello_func('Hi',name = 'Corey'))
+
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+student_info('Math', 'Art', name = 'John, age = 22')
