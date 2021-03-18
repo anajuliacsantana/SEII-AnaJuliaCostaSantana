6cde8a0acf8a9a1049e217d7d3e16e57e31fb735
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 11:52:17 -0300

    Módulo os-Status,data de modificação

--- Semana02/prog10.py
+++ Semana02/prog10.py
@@ -1,8 +1,9 @@
 import os
 
-
+from datetime import datetime
 os.chdir('/Users/anaju/Desktop')
 
-print(os.stat('demo.txt').st_size)
+mod_time = (os.stat('demo.txt').st_mtime)
+print(datetime.fromtimestamp(mod_time))
 
 # print(os.listdir())
