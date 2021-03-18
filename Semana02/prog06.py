3f153969b9187de741be95dab032570878313a77
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 09:09:32 -0300

    Condicionais com or

--- Semana02/prog06.py
+++ Semana02/prog06.py
@@ -2,13 +2,15 @@
 
 
 #and
+#Ambas as condições devem ser verdadeiras
 #or
+#Uma ou outra informação pode ser Verdadeira
 #not
 
 user = 'Admin'
 logged_in = False
 
-if user == 'Admin' and logged_in:
+if user == 'Admin' or logged_in:
     print('Admin Page')
 else:
     print('Bad Creds')
