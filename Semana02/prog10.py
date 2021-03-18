481af4a54adee96639c6f9b28e9f2960607cf72b
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 12:08:15 -0300

    Módulo os-Acessando o home

--- Semana02/prog10.py
+++ Semana02/prog10.py
@@ -3,8 +3,7 @@ import os
 from datetime import datetime
 os.chdir('/Users/anaju/Desktop')
 
-for dirpath, dirnames, filenames in os.walk('/Users/anaju/Desktop'):
-    print('Current Path:',dirpath)
-    print('Directories:',dirnames)
-    print('Files',filenames)
-    print()
+print(os.environ.get('HOMEPATH'))
+'test.txt'
+
+file_path = os.enciron.get('HOMEPATH') + 
