dfacc0c3806fe73003b81312ba43b0821e87c789
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 20:35:30 -0300

    Organizando-exemplo-empregados

--- Semana02/prog19.py
+++ Semana02/prog19.py
@@ -1,12 +1,16 @@
 
 
-li = [-6,-5,-4,1,2,3]
-s_li = sorted(li,key=abs)
-print(s_li)
+class Employee():
+    def __init__(self,name,age,salary):
+        self.name = name
+        self.age = age
+        sel.salary = salary
+    def __repr(self):
+        return'({},{},${})'.format(self.name, self.age, self.salary)
 
+e1 = Employee('Carl',37,70000)
+e2 = Employee('Sarah',29,80000)
+e3 = Employee('John',43,90000)
 
-
-
-
-
-
+employees = [e1,e2,e3]
+s_employees = sorted(employees)
