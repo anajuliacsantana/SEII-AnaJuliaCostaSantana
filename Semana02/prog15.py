e04463e8c96287565300e104cdcdd8edaa20738b
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 15:23:57 -0300

    Tabelas- trocando o  por tab

--- Semana02/prog15.py
+++ Semana02/prog15.py
@@ -4,7 +4,7 @@ with open('names.csv','r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     with open('new_names.csv','w') as new_file:
-        csv_writer = csv.writer(new_file, delimiter = '-')
+        csv_writer = csv.writer(new_file, delimiter = '\t')
         
         for line in csv_reader:
             csv_writer.writerow(line)
