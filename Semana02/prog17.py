41ce8b6d29cbb2e0353c36bc3b83a082b3477ed0
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 19:08:45 -0300

    Trabalhando com Datas

--- Semana02/prog17.py
+++ Semana02/prog17.py
@@ -1,6 +1,7 @@
 import datetime
 
 tday = datetime.date.today()
-print(tday.weekday()) #Monday 0 Sunday 6
-print(tday.isoweekday()) #Monday 1 Sunday 7
 
+tdelta = datetime.timedelta(days = 7)
+
+print(tday + tdelta)
