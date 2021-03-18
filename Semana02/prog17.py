d7da54d4881a63bb1c790edfad6532fa7bdc0673
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 19:12:21 -0300

    Saber quanto tempo falta

--- Semana02/prog17.py
+++ Semana02/prog17.py
@@ -4,4 +4,12 @@ tday = datetime.date.today()
 
 tdelta = datetime.timedelta(days = 7)
 
-print(tday - tdelta)
+# print(tday - tdelta)
+
+#date2 = date1 + timedelta
+#timedelta = date1 + date2
+
+bday = datetime.date(2022,3,2)
+
+till_bday = bday - tday
+print(till_bday)
