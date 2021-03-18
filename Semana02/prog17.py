94ed600c4889bd0762bf2329f3ff97b35572aebc
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 19:48:40 -0300

    Conversão datetime e string

--- Semana02/prog17.py
+++ Semana02/prog17.py
@@ -5,3 +5,9 @@ import pytz
 
 dt_mtn = datetime.datetime.now(tz=pytz.timezone('US/Mountain'))
 print(dt_mtn.strftime('%B %d, %Y'))
+
+dt_str = 'March 18, 2021'
+dt = datetime.datetime.strptime(dt_str,'%B %d, %Y')
+print(dt)
+#strftime - Datetime to String
+# srtptime - String to Datetime
