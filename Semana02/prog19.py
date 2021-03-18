b2265792db6e4bfa1df973231791094fc9f44562
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 20:23:24 -0300

    Organizando-Listas

--- Semana02/prog19.py
+++ Semana02/prog19.py
@@ -1,12 +1,8 @@
 
-li = [9,1,8,2,7,3,6,4,5]
-s_li = sorted(li,reverse = True)
-
-print('Sorted Variable:\t',s_li)
-li.sort(reverse = True)
-print('Original Variable:\t',li)
-
-
+#Sort metod works only in lists
+tup = (9, 1, 8, 2, 7, 3, 6, 4, 5)
+s_tup = sorted(tup)
+print('Tuple\t',s_tup)
 
 
 
