9d58cafa0b641f407836a2259f51142dc7141c2b
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 11:41:32 -0300

    Módulo os-Acessando e criando pastas

--- Semana02/prog10.py
+++ Semana02/prog10.py
@@ -1,3 +1,9 @@
 import os
 
-print(dir(os))
+
+os.chdir('/Users/anaju/Desktop')
+
+#os.mkdir('OS-Demo-2/Sub-Dir-1')##não cria subpastas
+os.makedirs('OS-Demo-2/OS-Demo-2/Sub-Dir-1')
+print(os.listdir())
+
