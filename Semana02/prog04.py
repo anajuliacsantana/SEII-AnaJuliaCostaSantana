c7de5e8f023aadd82ef8af0b1d51862513a428f3
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 01:57:22 -0300

    Organizando a lista-Sort2

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -1,7 +1,10 @@
 courses = ['History','Math','Physics','CompSci']
 
+nums = [1,5,2,4,3]
+
 courses.sort()
+nums.sort()
 
 print(courses)
-
+print(nums)
 #Organiza em ordem alfabética ou numeral crescente
