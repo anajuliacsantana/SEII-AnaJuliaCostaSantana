c1b614d7b40bdb804ac09f9c112a663fced49979
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 12:42:39 -0300

    Abrindo arquivos

--- Semana02/prog11.py
+++ Semana02/prog11.py
@@ -2,8 +2,10 @@
 
 with open('test.txt','r') as f:
     
-    f_contents = f.read(100)
-    print(f_contents, end ='')
-    f_contents = f.read(100)
-    print(f_contents, end ='')
-    
+    size_to_read = 10
+
+    f_contents = f.read(size_to_read)
+
+    while len(f_contents)>0:
+            print(f_contents, end ='*')
+            f_contents = f.read(size_to_read)
