950671cd9a62f9e354e68776cfc60e82828b5cd2
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 02:59:06 -0300

    Resumo Listas,Tuples e Sets

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -1,7 +1,12 @@
 
-# Sets
-cs_courses = {'History', 'Math', 'Physics', 'CompSci', 'Math'}
-art_courses = {'History', 'Math', 'Art', 'Design'}
+# Empty Lists
+empty_list = []
+empty_list = list()
 
+# Empty Tuples
+empty_tuple = ()
+empty_tuple = tuple()
 
-print(cs_courses.union(art_courses))
+# Empty Sets
+empty_set = {} # This isn't right! It's a dict
+empty_set = set()
