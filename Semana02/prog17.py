8600f7f5c79cbf46890d372af9adf7956f5fcfdd
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 19:15:15 -0300

    Tabalho co mtempo

--- Semana02/prog17.py
+++ Semana02/prog17.py
@@ -1,15 +1,4 @@
 import datetime
 
-tday = datetime.date.today()
-
-tdelta = datetime.timedelta(days = 7)
-
-# print(tday - tdelta)
-
-#date2 = date1 + timedelta
-#timedelta = date1 + date2
-
-bday = datetime.date(2022,3,2)
-
-till_bday = bday - tday
-print(till_bday)
+t = datetime.time(9,30,45,100000)
+print(t)
