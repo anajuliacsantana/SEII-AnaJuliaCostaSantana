35f7e94f8bd468d3725c6d574b3ea3d4ba1e2b1f
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 20:36:03 -0300

    Organizando-exemplo-empregados

--- Semana02/prog19.py
+++ Semana02/prog19.py
@@ -4,7 +4,7 @@ class Employee():
     def __init__(self,name,age,salary):
         self.name = name
         self.age = age
-        sel.salary = salary
+        self.salary = salary
     def __repr(self):
         return'({},{},${})'.format(self.name, self.age, self.salary)
 
