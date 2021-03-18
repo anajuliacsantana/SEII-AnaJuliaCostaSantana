b10165461a8c1c34dbaa799de0df28b38add253f
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 16:19:39 -0300

    Removendo nomes

--- Semana02/prog16.py
+++ Semana02/prog16.py
@@ -6,9 +6,15 @@ names = []
 with open('patrons.csv','r') as data_file:
     csv_data = csv.reader(data_file)
 
-    for line in csv_data:
-        print(line)
+    #We dont' want headers or first line of band data
+    next(csv_data)
+    next(csv_data)
 
+   
+    for line in csv_data:
+        if line[0] == 'No Reward':
+            break
+        names.append(f"{line[0]} {line[1]}" ) 
 
 
 
