08000862c222ebec6704335080d73aa6491d0399
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 13:01:50 -0300

    Copiando imagens

--- Semana02/prog11.py
+++ Semana02/prog11.py
@@ -2,5 +2,8 @@
 
 with open('panda-deitado.jpg','rb') as rf:
    with open('panda-deitado-copy.jpg','wb') as wf:
-       for line in rf:
-           wf.write(line)
+      chunk_size = 4096
+      rf_chunk = rf.read(chunk_size)
+      while len(rf_chunk) > 0:
+          wf.write(rf_chunk)
+          rf_chunk= rf.read(chunk_size)
