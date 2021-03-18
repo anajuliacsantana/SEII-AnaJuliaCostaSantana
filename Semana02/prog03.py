84cd7e5be462eab960def05bef8dc86a8496fac0
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-17 21:34:12 -0300

    Comparadores-Igualdade

--- Semana02/prog03.py
+++ Semana02/prog03.py
@@ -1,12 +1,13 @@
 
-# Arithmetic Operators:
-# Addition:       3 + 2
-# Subtraction:    3 - 2
-# Multiplication: 3 * 2
-# Division:       3 / 2
-# Floor Division: 3 // 2
-# Exponent:       3 ** 2
-# Modulus:        3 % 2
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+#Retornam valores Booleanos
+num_1 = 3
+num_2 = 2
 
-#round arredonda o sinal para o valor inteiro mais próximo
-print(round(3.75,1))
+print(num_1 ==num_2)
