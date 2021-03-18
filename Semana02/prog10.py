ffef380458a7cedd05c34ad35ba1c1fd2877b918
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 11:48:52 -0300

    Módulo os-Status

--- Semana02/prog10.py
+++ Semana02/prog10.py
@@ -3,7 +3,6 @@ import os
 
 os.chdir('/Users/anaju/Desktop')
 
-#os.mkdir('OS-Demo-2/Sub-Dir-1')##não cria subpastas
-os.makedirs('OS-Demo-2/OS-Demo-2/Sub-Dir-1')
-print(os.listdir())
+print(os.stat('demo.txt'))
 
+# print(os.listdir())
