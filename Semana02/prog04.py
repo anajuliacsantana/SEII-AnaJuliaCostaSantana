74064a065d9fba12ae8bd2125dcaf2bfa974b7bf
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 02:29:16 -0300

    Tuples1

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -1,8 +1,27 @@
-courses = ['History','Math','Physics','CompSci']
 
-course_str = ' - '.join(courses)
+#tuples são não mutáveis
 
-new_list = course_str.split(' - ')
+# Mutable
+list_1 = ['History', 'Math', 'Physics', 'CompSci']
+list_2 = list_1
 
-print(course_str)
-print(new_list)  
+print(list_1)
+print(list_2)
+
+list_1[0] = 'Art'
+
+print(list_1)
+print(list_2)
+
+
+# Immutable
+# tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+# tuple_2 = tuple_1
+
+# print(tuple_1)
+# print(tuple_2)
+
+# tuple_1[0] = 'Art'
+
+# print(tuple_1)
+# print(tuple_2)
