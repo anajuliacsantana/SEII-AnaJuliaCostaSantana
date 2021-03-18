34abb1afbcbe5c2946c46fd5e04c1dd0d626980e
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 12:11:43 -0300

    Módulo os-Join no path

--- Semana02/prog10.py
+++ Semana02/prog10.py
@@ -4,6 +4,9 @@ from datetime import datetime
 os.chdir('/Users/anaju/Desktop')
 
 print(os.environ.get('HOMEPATH'))
+
 'test.txt'
 
-file_path = os.enciron.get('HOMEPATH') + 
+file_path = os.path.join(os.environ.get('HOMEPATH'),'test.txt')
+print(file_path)
+
