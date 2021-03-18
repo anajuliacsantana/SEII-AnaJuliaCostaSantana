649458cd57ddf3a1272a0a8466de247c8bcaa9f5
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 01:37:09 -0300

    Intervalo de valores com index

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -1,7 +1,9 @@
 courses = ['History','Math','Physics','CompSci']
 
-print(courses[4])
-
+print(courses[0:2])
+#Printa todos os valores de 0 e para antes do segundo index
+#por isso o intervalo apresenta 2 itens
+#pode ser iniciada como :2
 
 
 
