43a102d92afc698330e796ff4ddf5458c43d5849
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 20:27:51 -0300

    Organizando-Negativos

--- Semana02/prog19.py
+++ Semana02/prog19.py
@@ -1,14 +1,8 @@
 
-#Sort metod works only in lists, logo converte os outros dados
-# tup = (9, 1, 8, 2, 7, 3, 6, 4, 5)
-# s_tup = sorted(tup)
-# print('Tuple\t',s_tup)
 
-di = {'name':'Corey','job':'programming','age': None, 'os':'Mac'}
-s_di = sorted(di)
-print('Dict\t',s_di)
-
-
+li = [-6,-5,-4,1,2,3]
+s_li = sorted(li)
+print(s_li)
 
 
 
