bb250a288faa0fb4d1882c25977dfe37f03cee47
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 01:58:58 -0300

    Organizando em ordem decrescente

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -2,9 +2,8 @@ courses = ['History','Math','Physics','CompSci']
 
 nums = [1,5,2,4,3]
 
-courses.sort()
-nums.sort()
+courses.sort(reverse = True)
+nums.sort(reverse = True)
 
 print(courses)
 print(nums)
-#Organiza em ordem alfabética ou numeral crescente
