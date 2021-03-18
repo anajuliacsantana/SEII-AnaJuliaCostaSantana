4fb021c6d20da7472fc2de82192e5957696aae02
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 01:49:02 -0300

    Removendo valores da lista-Remove

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -1,7 +1,6 @@
 courses = ['History','Math','Physics','CompSci']
 
-courses_2 = ['Art','Education']
-courses.insert(0,courses_2)
-#Adiciona toda a ista e não os valores individuais
-print(courses[0])
+
+courses.remove('Math')
+print(courses)
 
