c7b29015d2dfe2285e62f8cdd751de272c020adb
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 21:12:47 -0300

    Blocos try/except,else,finally,raise

--- Semana02/prog20.py
+++ Semana02/prog20.py
@@ -1,8 +1,8 @@
 
 try:
     f = open('currupt_file.txt')
-    if f.name =='currupt_file.txt':
-        raise Exception
+    # if f.name =='currupt_file.txt':
+    #     raise Exception
 except FileNotFoundError as e:
     print(e)
 except Exception as e:
