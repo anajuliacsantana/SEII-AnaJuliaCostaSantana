c32df37e95051036d42a595dd640539c2bef347b
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 21:44:14 -0300

    Gráfico Básico-Formatações

--- Semana02/matplot01.py
+++ Semana02/matplot01.py
@@ -5,7 +5,14 @@ import pandas as pd
 
 ##Basic Graph
 
-x = [1,2,3]
-y = [2,4,6]
+x = [0,1,2,3,4]
+y = [0,2,4,6,8]
 plt.plot(x,y)
+
+plt.title('Our First Graph!',fontdict={'fontname':'Comic Sans MS','fontsize':20})
+plt.xlabel('X Axis!')
+plt.ylabel('Y Axis!')
+
+plt.xticks([0,1,2,3])
+plt.yticks([0,2,4,6,8,10])
 plt.show()
