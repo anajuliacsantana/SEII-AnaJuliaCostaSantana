ecedbf490d6282f3729fa390b58a85a9005b3b2e
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 22:17:20 -0300

    Matplot Básico

--- Semana02/matplot01.py
+++ Semana02/matplot01.py
@@ -8,30 +8,44 @@ import pandas as pd
 x = [0,1,2,3,4]
 y = [0,2,4,6,8]
 
-#Resize your Graph
+#Resize your Graph(dpi - pixels por polegada)
 plt.figure(figsize = (5,3),dpi=100)
 
+##Line 1
+
+#Keyword Argument Notation
 # plt.plot(x,y,label ='2x', color ='red',linewidth = 2, marker='.',linestyle = '--',markersize = 10, markeredgecolor = 'blue')
 
-# Shorthand notation
+#Shorthand notation
 #fmt = '[color][marker][line]'
 plt.plot(x,y, 'b^--', label='2x')
 
 ##Line Number Two
-x2 = np.arange(0,4.5,0.5)
-plt.plot(x2[:6],x2[:6]**2,'r', label = 'X^2')
 
+#Select interval of points
+x2 = np.arange(0,4.5,0.5)
+
+#Plot parte do gráfico como linha
+plt.plot(x2[:6],x2[:6]**2,'r', label = 'X^2')
+#O restante pontilhado
 plt.plot(x2[5:],x2[5:]**2,'r--')
 
+#Adiciona título, formatações diversas
 plt.title('Our First Graph!',fontdict={'fontname':'Comic Sans MS','fontsize':20})
+
+#Label dos eixos x e y
 plt.xlabel('X Axis!')
 plt.ylabel('Y Axis!')
 
-plt.xticks([0,1,2,3])
+#As escalas do gráfico
+plt.xticks([0,1,2,3,4])
 # plt.yticks([0,2,4,6,8,10])
 
+#Legenda
 plt.legend()
 
+#Salvando a figura, se possível dpi >= 300
 plt.savefig('mygraph.png', dpi = 300)
 
+#Mostra o gráfico
 plt.show()
