dc6c54ae1b2cae546c079d6b1049837f11cbb574
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 22:08:11 -0300

    Nova linha-Aumentando o gráfico

--- Semana02/matplot01.py
+++ Semana02/matplot01.py
@@ -7,6 +7,10 @@ import pandas as pd
 
 x = [0,1,2,3,4]
 y = [0,2,4,6,8]
+
+#Resize your Graph
+plt.figure(figsize = (5,3),dpi=300)
+
 # plt.plot(x,y,label ='2x', color ='red',linewidth = 2, marker='.',linestyle = '--',markersize = 10, markeredgecolor = 'blue')
 
 # Shorthand notation
@@ -14,15 +18,17 @@ y = [0,2,4,6,8]
 plt.plot(x,y, 'b^--', label='2x')
 
 ##Line Number Two
-x2 = np. arrange(0,4.5,0.5)
-print(x2)
+x2 = np.arange(0,4.5,0.5)
+plt.plot(x2[:6],x2[:6]**2,'r', label = 'X^2')
+
+plt.plot(x2[5:],x2[5:]**2,'r--')
 
 plt.title('Our First Graph!',fontdict={'fontname':'Comic Sans MS','fontsize':20})
 plt.xlabel('X Axis!')
 plt.ylabel('Y Axis!')
 
 plt.xticks([0,1,2,3])
-plt.yticks([0,2,4,6,8,10])
+# plt.yticks([0,2,4,6,8,10])
 
 plt.legend()
 
