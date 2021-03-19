ae4b0258c8a1b8ebcb112d2e3d3b6382e80e3d8c
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 21:57:18 -0300

    Atalhos

--- Semana02/matplot01.py
+++ Semana02/matplot01.py
@@ -7,8 +7,11 @@ import pandas as pd
 
 x = [0,1,2,3,4]
 y = [0,2,4,6,8]
-plt.plot(x,y,label ='2x', color ='red',linewidth = 2, marker='.',linestyle = '--',markersize = 10, markeredgecolor = 'blue')
+# plt.plot(x,y,label ='2x', color ='red',linewidth = 2, marker='.',linestyle = '--',markersize = 10, markeredgecolor = 'blue')
 
+# Shorthand notation
+#fmt = '[color][marker][line]'
+plt.plot(x,y, 'b^--', label='2x')
 plt.title('Our First Graph!',fontdict={'fontname':'Comic Sans MS','fontsize':20})
 plt.xlabel('X Axis!')
 plt.ylabel('Y Axis!')
