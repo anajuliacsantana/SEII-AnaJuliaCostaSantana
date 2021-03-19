f1f0a3876f4d7d70dff6a91bef389a83c190e8ed
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-18 21:49:27 -0300

    Gráfico Básico-Formatações

--- Semana02/matplot01.py
+++ Semana02/matplot01.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 x = [0,1,2,3,4]
 y = [0,2,4,6,8]
-plt.plot(x,y,label ='2x', color ='red')
+plt.plot(x,y,label ='2x', color ='red',linewidth = 2, marker='.',linestyle = '--' markersize = 10, markeredgecolor = 'blue')
 
 plt.title('Our First Graph!',fontdict={'fontname':'Comic Sans MS','fontsize':20})
 plt.xlabel('X Axis!')
