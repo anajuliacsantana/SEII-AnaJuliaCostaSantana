fc82656ae7eec6db23d7bed2912cdc2f50bb84ff
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-27 19:07:55 -0300

    Atualizando e corriginfo

--- Semana03/codigo01.cpp
+++ Semana03/codigo01.cpp
@@ -6,6 +6,13 @@ using namespace std;
 
 #include "complexos.h"
 
+struct Complexo soma(Complexo numeros[],int n_soma);
+void printComplex ( Complexo nums_complexs[],int n);
+void printResult(Complexo nums_complexs);
+struct Complexo subt(Complexo numeros[],int n_soma);
+struct Complexo div(Complexo numeros[],int n_soma);
+struct Complexo multiplx(Complexo numeros[],int n_soma);
+struct Complexo conversao(Complexo numeros[],int n_soma);
 
 int main () {
     unsigned int N;
@@ -13,7 +20,7 @@ int main () {
     printf("Quantos numeros?\n");
 	scanf("%u", &N);
     getchar();
-    Complexo num_complexo[N];
+    Complexo num_complexo[N], somas;
     cout << "Insira o número, parte real( espaço ou enter),parte imaginária na forma retangular:\n ";
     for(i=0;i<N;i++)
 	{   printf("Número %u: \n", i+1);
@@ -26,7 +33,9 @@ int main () {
 	scanf("%d",&op);
 	switch(op){
 		case 1:{
-
+			somas = soma(num_complexo,N);
+			printf("O resultado da soma é: ");
+			printResult(somas);
             break;
               }
            
@@ -46,6 +55,7 @@ int main () {
 		
         case 6:
         { 
+		printComplex(num_complexo,N);
         break;}
 
 		break;
@@ -54,11 +64,43 @@ int main () {
  	    {
  		if(op!= 7)
  		printf("Operacao invalida!\n");
- 		break;
-    }
+ 		break; }
   }	
 }while(op!=7);
      return 0;
 }
 
+struct Complexo soma(Complexo numeros[],int n_soma){
+	struct Complexo aux_soma = {};
+	int i;
+	float soma_real = 0;
+	float soma_imag = 0;
+	for(i = 0 ;i<n_soma;i++){
+	aux_soma.real += numeros[i].real;
+	aux_soma.imag += numeros[i].imag;
+	}
+	return aux_soma;
+}
 
+void printResult(Complexo nums_complexs){ 
+    if(nums_complexs.imag > 0){
+	printf("%f + %f j\n",nums_complexs.real,nums_complexs.imag);
+	}
+	else if(nums_complexs.imag == 0) 
+	printf("%f \n",nums_complexs.real);
+	else 
+	printf("%f %f j\n",nums_complexs.real,nums_complexs.imag);
+	}
+
+void printComplex(Complexo nums_complexs[],int n){ 
+	int i;
+	for(i=0;i<n;i++){
+    if(nums_complexs[i].imag > 0){
+	printf("Número %d : %f +%fj\n",i+1,nums_complexs[i].real,nums_complexs[i].imag);
+	}
+	else if(nums_complexs[i].imag == 0) 
+	printf("Número %d : %f \n",i+1,nums_complexs[i].real);
+	else 
+	printf("Número %d : %f %fj\n",i+1,nums_complexs[i].real,nums_complexs[i].imag);
+	}
+}
