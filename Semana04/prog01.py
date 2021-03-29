7e7ff0ba4dc15f36b151907d681e6f317bb298e1
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-29 10:56:41 -0300

    Prog01

--- Semana04/prog01.py
+++ Semana04/prog01.py
@@ -11,6 +11,8 @@ def do_something(seconds):
 with concurrent.futures.ThreadPoolExecutor() as executor:
     secs = [5,4,3,2,1]
     results = executor.map(do_something,secs)
+    for result in results:
+        print(result)
 
 # threads = []
 
