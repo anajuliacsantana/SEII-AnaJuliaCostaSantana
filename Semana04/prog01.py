55a3b66f2f055ba43b9a7fe3e5f496c2ecc45612
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-29 09:17:11 -0300

    a

--- Semana04/prog01.py
+++ Semana04/prog01.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 start = time.perf_counter()
@@ -5,10 +6,10 @@ start = time.perf_counter()
 def do_something():
     print('Sleeping 1 second...')
     time.sleep(1)
-    print('Done sleeping...')
+    print('Done Sleeping...')
 
-do_something()
-do_something()
+t1 = threading.Thread(target=do_something)
+t2 = threading.Thread(target=do_something)
 
 finish = time.perf_counter()
 
