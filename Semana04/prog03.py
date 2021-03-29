ec3f4a696fae81cdfe17402b17b83bf28d0f2c95
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-29 11:46:22 -0300

    Using the Multiprocessing Module

--- Semana04/prog03.py
+++ Semana04/prog03.py
@@ -8,12 +8,16 @@ def do_something():
     time.sleep(1)
     print('Done Sleeping...')
 
-
+if __name__ == '__main__':
 p1 = multiprocessing.Process(target = do_something)
-p2 =  multiprocessing.Process(target = do_something)
+p2 = multiprocessing.Process(target = do_something)
 
-p1.star()
+p1.start()
 p2.start()
+
+p1.join()
+p2.join()
+
 finish = time.perf_counter()
 
 print(f'Finished in{round(finish-start,2)} second(s)')
