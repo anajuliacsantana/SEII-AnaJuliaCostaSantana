324e284cd352cb36c8945b8cbd8b5c64ad954030
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-29 10:54:38 -0300

    Prog01

--- Semana04/prog01.py
+++ Semana04/prog01.py
@@ -6,10 +6,11 @@ start = time.perf_counter()
 def do_something(seconds):
     print(f'Sleeping {seconds} second(s)...')
     time.sleep(seconds)
-    return 'Done Sleeping...'
+    return f'Done Sleeping...{seconds}'
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = [executor.submit(do_something,1) for _ in range(10)]
+    secs = [5,4,3,2,1]
+    results = [executor.submit(do_something,sec) for sec in secs]
     
     for f in concurrent.futures.as_completed(results):
         print(f.result())
