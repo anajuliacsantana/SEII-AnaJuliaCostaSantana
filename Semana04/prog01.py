6ebb062a8816c988c4e1b3f76209d5851553ae5d
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-29 09:20:47 -0300

    a

--- Semana04/prog01.py
+++ Semana04/prog01.py
@@ -6,10 +6,16 @@ start = time.perf_counter()
 def do_something():
     print('Sleeping 1 second...')
     time.sleep(1)
-    print('Done Sleeping...')
+    print('Done Sleeping...\n')
 
 t1 = threading.Thread(target=do_something)
 t2 = threading.Thread(target=do_something)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
 
 finish = time.perf_counter()
 
