ef61b06aec74490434d75726a7036c3f5096c4e9
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-30 10:49:13 -0300

    Using the Multiprocessing Module

--- Semana04/prog03.py
+++ Semana04/prog03.py
@@ -3,15 +3,16 @@ import multiprocessing
 
 start = time.perf_counter()
 
-def do_something():
-    print('Sleeping 1 second...')
-    time.sleep(1)
+def do_something(seconds):
+    print(f'Sleeping {seconds} second...')
+    time.sleep(seconds)
     print('Done Sleeping...')
 
 processes = []
+
 for _ in range(10):
-    
-   p = multiprocessing.Process(target = do_something)
+
+   p = multiprocessing.Process(target = do_something, args =[1.5])
    p.start()
    processes.append(p)
 
