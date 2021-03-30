7e46c7b22f98a2b6af0b9bbd8066bdcd409d578c
Ana Júlia Santana <anajulia.costasantana@gmail.com>
2021-03-30 11:15:20 -0300

    Using the Multiprocessing Module

---
+++ Semana04/prog02.py
@@ -0,0 +1,42 @@
+import time
+from PIL import Image, ImageFilter #bilbioteca de imagens para python
+
+img_names = [
+    'photo-1516117172878-fd2c41f4a759.jpg',
+    'photo-1532009324734-20a7a5813719.jpg',
+    'photo-1524429656589-6633a470097c.jpg',
+    'photo-1530224264768-7ff8c1789d79.jpg',
+    'photo-1564135624576-c5c88640f235.jpg',
+    'photo-1541698444083-023c97d3f4b6.jpg',
+    'photo-1522364723953-452d3431c267.jpg',
+    'photo-1513938709626-033611b8cc03.jpg',
+    'photo-1507143550189-fed454f93097.jpg',
+    'photo-1493976040374-85c8e12f0c0e.jpg',
+    'photo-1504198453319-5ce911bafcde.jpg',
+    'photo-1530122037265-a5f1f91d3b99.jpg',
+    'photo-1516972810927-80185027ca84.jpg',
+    'photo-1550439062-609e1531270e.jpg',
+    'photo-1549692520-acc6669e2f0c.jpg'
+]
+#ftos que foram baixadas
+
+t1 = time.perf_counter() #iniciando o contador
+
+size = (1200, 1200) #tamanho em pixels das fotos que serão redimensionadas
+
+
+for img_name in img_names:
+    img = Image.open(img_name) #abrindo a imagem
+
+    img = img.filter(ImageFilter.GaussianBlur(15)) # passando a imagem num filtro
+    
+    img.thumbnail(size) #criando uma miniatura da imagem
+    img.save(f'processed/{img_name}') #salvando a imagem no arquivo de imagens processadas
+    print(f'{img_name} was processed...') #fala qual imagem já foi porcessada
+
+
+t2 = time.perf_counter()
+
+print(f'Finished in {t2-t1} seconds')
+
+#dessa forma apenas uma foto é processada por vez
